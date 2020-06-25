735e316432eaf82ce2a76f73e3222e4ab3650f5c
dangeloguillermo@gmail.com <Fermin.19>
2020-06-25 17:59:36 -0300

    cambios

--- pandoc_export_pdf.sh
+++ pandoc_export_pdf.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 pandoc "$1"\
-	--include-in-header "/home/guillermo/Documentos/git/tesis/markdown/latex_header.tex" \
-	-V papersize:a4 \
-	-V geometry:margin=2.5cm \
-	-V fontsize=11pt \
-    	-V mainfont="Liberation Serif" \
-	-V linkcolor:blue \
 	--bibliography "markdown/bibliografia.bib" \
 	--csl "markdown/apa.csl" \
 	--pdf-engine=xelatex \
-	-V lang=es \
 	-o "$2"
