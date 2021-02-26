6ff62e683e9bd2bb1d0fb749b9f1180d870a28e3
Guillermo D'Angelo <dangeloguillermo@gmail.com>
2021-02-26 18:58:14 -0300

    CAMBIOS

--- functions/agrupar_dfs_censo.py
+++ functions/agrupar_dfs_censo.py
@@ -56,3 +56,40 @@ def agrupar_por_edades(df, col_edad):
     df_group_pegue = col_pegue.merge(df_group, how='left', on='edad')
     
     return df_group_pegue
+
+
+def grupos_de_edad(df, col_edad):
+    "Agrupa por grupos de edad"
+    import pandas as pd
+    import numpy as np
+    
+    # agrupa
+    df_group = df.groupby([col_edad]).size().reset_index()
+    # renombra vars
+    df_group.rename(columns={col_edad: 'edad', 0:'personas'}, inplace=True)
+    # calcula porcentajes
+    df_group['porc_pers'] = (df_group.personas / df_group.personas.sum())*100
+    # genera vectores de edades del 0 asl 111, para pegar   
+    col_pegue = pd.DataFrame(np.arange(0, 112), columns=['edad'])
+    # pega con edades
+    df_group_pegue = col_pegue.merge(df_group, how='left', on='edad')
+    
+    return df_group_pegue
+
+
+def grupos_de_edad(df, col_edad):
+    " Calcula porcentaje de personas por grupos de edad: 0 a 3, 4 a 17, 18 a 64 y >64 "
+    import pandas as pd
+    import numpy as np
+    df.loc[df[col_edad].between(0,   3),  'grupo_edad'] = 1
+    df.loc[df[col_edad].between(4, 17),  'grupo_edad'] = 2
+    df.loc[df[col_edad].between(18, 64),  'grupo_edad'] = 3
+    df.loc[df[col_edad] > 64,  'grupo_edad'] = 4
+    df_group = df.groupby(['grupo_edad']).size().reset_index()
+    
+    # renombra vars
+    df_group.rename(columns={0:'personas'}, inplace=True)
+    # calcula porcentajes
+    df_group['porc_pers'] = (df_group.personas / df_group.personas.sum())*100
+        
+    return df_group.porc_pers.values.tolist()
