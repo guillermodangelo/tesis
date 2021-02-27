955fa66076318d251d66a1dfdfd9ff0dcfed1b8f
Guillermo D'Angelo <dangeloguillermo@gmail.com>
2021-02-27 17:55:56 -0300

    cambios

--- functions/.ipynb_checkpoints/cargar_data-checkpoint.py
+++ functions/.ipynb_checkpoints/cargar_data-checkpoint.py
@@ -1,10 +1,7 @@
 def cargar_data_eda():
     "Carga datos a ser usados en el EDA"
-    
     import pandas as pd
-    import numpy as np
-    import geopandas as gpd
-    
+    import numpy as np    
     # Datos censales
     censo = pd.read_csv('tablas/personas_censo_2011.gz', compression='gzip', header=0, sep=',', quotechar='"')
     # reemplaza el valor 5555 en edad (variable PERNA01) por NaNs
@@ -18,3 +15,32 @@ def cargar_data_eda():
     md.drop(['latlon_ori', 'latlon_des'], axis=1, inplace=True)
 
     return censo, pbi, md
+
+
+def cargar_data_metod():
+    "Carga datos a ser usados en el apartado metodológico"
+    import pandas as pd
+    import numpy as np
+    import geopandas as gpd    
+    # Datos censales
+    censo = pd.read_csv('tablas/personas_censo_2011.gz', compression='gzip', header=0, sep=',', quotechar='"')
+    # reemplaza el valor 5555 en edad (variable PERNA01) por NaNs
+    censo.loc[censo.PERNA01 == 5555, 'PERNA01'] = np.nan
+    # PBI departamental
+    pbi = pd.read_csv('tablas/pbi_departamental.csv')
+    # matriz de distancias
+    md = pd.read_csv('tablas/df_distancias_centro_poblacion.csv')
+    md.drop(['latlon_ori', 'latlon_des'], axis=1, inplace=True)
+    # carga capa departamentos INE pg
+    deptos = gpd.read_file('capas/ine_deptos.gpkg')
+    # agrega centroides de departamentos
+    deptos['centroide'] = deptos['geometry'].centroid
+    # carga capa localidades INE pt
+    localidad = gpd.read_file('capas/ine_localidades.gpkg')
+    localidad.CODLOC = localidad.CODLOC.astype(int)
+    # centro de población
+    centro_pobl = gpd.read_file('capas/centro_poblacion.gpkg')
+    # filtra capitales departamentales de las localidades INE
+    capital = localidad[localidad.CAPITAL==True].reset_index(drop=True)
+    
+    return censo, pbi, md, deptos, localidad, centro_pobls, capital
