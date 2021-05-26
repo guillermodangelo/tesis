70071e190da7c69e9e9002e1b6ed7119baf66c81
Guillermo D'Angelo <dangeloguillermo@gmail.com>
2021-05-26 11:46:07 -0300

    cambios en R

---
+++ 06-MIE_departamentos.R
@@ -0,0 +1,28 @@
+library(tidyr)
+
+# modelo poisson R
+dd_deptos <- read.csv('tablas/dd_deptos.csv')
+
+
+# reemplaza nulos dell largo del límite por 0.0001
+dd_deptos <- dd_deptos %>% replace_na(list(largo_limite = 0.0001))
+
+
+# model poisson regression using glm()
+model <- glm(personas_mig ~ nom_depto_orig + dummy_limit + log(largo_limite) +
+               log_pbi_destino + pbi_porcen_des + log_dist -1,
+               family = poisson(link = "log"),
+               data = dd_deptos)
+
+# resumen
+summary(model)
+
+# estimación
+dd_deptos$fitted <- round(fitted(model),0)
+
+
+
+
+
+dd_deptos$fitted
+
