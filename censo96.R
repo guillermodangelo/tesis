32cd19ff9794c5c96b3ee360b970a0fd6f509a50
Guillermo D'Angelo <dangeloguillermo@gmail.com>
2021-08-04 11:37:17 -0300

    Update censo96.R

--- censo96.R
+++ censo96.R
@@ -1,44 +1,68 @@
+library(foreign)
+library(haven)
+
+# con el SPSS
 ruta = '/mnt/hd/censo1996/MICRODATOS SPSS=CD/'
 setwd(ruta)
-
-library(foreign)
-
 path = file.path(ruta, "pob_96.sav")
 censo = read.spss(path, to.data.frame=TRUE)
 
-
-setwd("/mnt/hd/censo1996/")
-
-
-
-
-
-save(censo, file="personas_censo_96.RData")
-
-
-
 unique(censo$hace5aqui)
-
 unique(censo$hac5depc)
 
-
-censo$hac5depc_int = strtoi(censo$hac5depc)
-
-colnames(censo)
-
+# convierte a integer el depto de origen
 unique(censo$hac5depc_int)
 
-
 flujos_deptos96 = subset(censo, hace5aqui!='2')
-
-flujos_deptos96 = subset(flujos_deptos96, hac5depc_int<=19)
-
-
-
 unique(flujos_deptos96$hac5depc_int)
-unique(flujos_deptos96$hace5aqui)
 
-flujos_deptos96 = flujos_deptos96[flujos_deptos96$dpto != flujos_deptos96$hace5aqui, ]
+eliminar = c('  ', '30','99','40','70','35','31','32','80','90','60', '99')
+
+flujos_deptos96 <- flujos_deptos96[!flujos_deptos96$hac5depc %in% eliminar, ]
+
+unique(flujos_deptos96$hac5depc)
+unique(flujos_deptos96$dpto)
+
+
+flujos_deptos96 = flujos_deptos96[flujos_deptos96$dpto != flujos_deptos96$hac5depc, ]
+
+nrow(flujos_deptos96)
+# 193567 flujos, algo está mal
+
+
+
+
+#### con el DBF
+ruta = '/mnt/hd/censo1996/MICRODATOS DBF=CD/'
+setwd(ruta)
+path = file.path(ruta, "pob_96.DBF")
+censo = read.dbf(path)
+
+unique(censo$HACE5AQUI)
+unique(censo$HAC5DEPC)
+
+censo$HACE5AQUI = as.character(censo$HACE5AQUI)
+censo$HAC5DEPC = as.character(censo$HAC5DEPC)
+censo$DPTO = as.character(censo$DPTO)
+
+unique(censo$HAC5DEPC)
+unique(censo$DPTO)
+
+flujos_deptos96 = subset(censo, is.na(censo$HACE5AQUI))
+
+
+eliminar = c('30','99','40','70','35','31','32','80','90','60','99')
+
+flujos_deptos96 = flujos_deptos96[!flujos_deptos96$HAC5DEPC %in% eliminar, ]
+flujos_deptos96 = flujos_deptos96[complete.cases(flujos_deptos96$HAC5DEPC), ]
+
+flujos_deptos96 = flujos_deptos96[flujos_deptos96$DPTO != flujos_deptos96$HAC5DEPC, ]
+
+
+unique(flujos_deptos96$HAC5DEPC)
+
+unique(flujos_deptos96$DPTO)
 
 nrow(flujos_deptos96)
 
+table(flujos_deptos96$HAC5DEPC, flujos_deptos96$DPTO)
