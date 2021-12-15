09b667e9c39463ebd47c4124f668ee1e0777862b
Guillermo D'Angelo <dangeloguillermo@gmail.com>
2021-12-15 10:48:27 -0300

    ch

--- pandoc_export_word.sh
+++ pandoc_export_word.sh
@@ -5,6 +5,6 @@ pandoc "$1"\
 	--csl "markdown/apa-no-ampersand.csl" \
 	--filter pandoc-crossref \
 	--citeproc \
-	--reference-doc "markdown/reference.docx" \
+	--reference-doc "markdown/reference_v2.docx" \
 	--lua-filter="markdown/pagebreak.lua" \
 	-o "$2"
